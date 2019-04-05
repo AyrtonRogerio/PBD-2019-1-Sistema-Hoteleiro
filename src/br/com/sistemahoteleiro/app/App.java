3574f3d868106ac761f1edf35a0840fd20048b33
AyrtonRogerio <ayrton.rogerio1@gmail.com>
2019-04-04 21:55:16 -0300

    [Criação dos pacotes]

---
+++ src/br/com/sistemahoteleiro/app/App.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package br.com.sistemahoteleiro.app;
+
+/**
+ * @author ayrtons
+ *
+ */
+public class App {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
