bea4008bbb38ac48c691473be3c41bc3d7749f06
AyrtonRogerio <ayrton.rogerio1@gmail.com>
2019-04-29 16:28:27 -0300

    [Modificação na Facade, ControlerQuarto e Frames Home e Quarto.]

--- src/br/com/sistemahoteleiro/controler/ControlerQuarto.java
+++ src/br/com/sistemahoteleiro/controler/ControlerQuarto.java
@@ -6,7 +6,15 @@ package br.com.sistemahoteleiro.controler;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 
 /**
  * @author ayrtons
@@ -14,6 +22,102 @@ import javafx.fxml.Initializable;
  */
 public class ControlerQuarto implements Initializable {
 
+	
+	@FXML // fx:id="listaQuartosTab"
+    private Tab listaQuartosTab; // Value injected by FXMLLoader
+
+    @FXML // fx:id="pesquisaField"
+    private TextField pesquisaField; // Value injected by FXMLLoader
+
+    @FXML // fx:id="pesquisaButton"
+    private Button pesquisaButton; // Value injected by FXMLLoader
+
+    @FXML // fx:id="tabelaQuarto"
+    private TableView<?> tabelaQuarto; // Value injected by FXMLLoader
+
+    @FXML // fx:id="quartoCollum"
+    private TableColumn<?, ?> quartoCollum; // Value injected by FXMLLoader
+
+    @FXML // fx:id="qtCamasCollum"
+    private TableColumn<?, ?> qtCamasCollum; // Value injected by FXMLLoader
+
+    @FXML // fx:id="tipoColum"
+    private TableColumn<?, ?> tipoColum; // Value injected by FXMLLoader
+
+    @FXML // fx:id="tvCaboColum"
+    private TableColumn<?, ?> tvCaboColum; // Value injected by FXMLLoader
+
+    @FXML // fx:id="cofreColum"
+    private TableColumn<?, ?> cofreColum; // Value injected by FXMLLoader
+
+    @FXML // fx:id="tvLedColum"
+    private TableColumn<?, ?> tvLedColum; // Value injected by FXMLLoader
+
+    @FXML // fx:id="telColum"
+    private TableColumn<?, ?> telColum; // Value injected by FXMLLoader
+
+    @FXML // fx:id="arCondColum"
+    private TableColumn<?, ?> arCondColum; // Value injected by FXMLLoader
+
+    @FXML // fx:id="wifiColum"
+    private TableColumn<?, ?> wifiColum; // Value injected by FXMLLoader
+
+    @FXML // fx:id="miniBarColum"
+    private TableColumn<?, ?> miniBarColum; // Value injected by FXMLLoader
+
+    @FXML // fx:id="disponivelColum"
+    private TableColumn<?, ?> disponivelColum; // Value injected by FXMLLoader
+
+    @FXML // fx:id="editarButton"
+    private Button editarButton; // Value injected by FXMLLoader
+
+    @FXML // fx:id="novoAlugelTab"
+    private Tab novoAlugelTab; // Value injected by FXMLLoader
+
+    @FXML // fx:id="numQuartoField"
+    private TextField numQuartoField; // Value injected by FXMLLoader
+
+    @FXML // fx:id="qtdCamaField"
+    private TextField qtdCamaField; // Value injected by FXMLLoader
+
+    @FXML // fx:id="tipoField"
+    private TextField tipoField; // Value injected by FXMLLoader
+
+    @FXML // fx:id="tvCaboCBox"
+    private CheckBox tvCaboCBox; // Value injected by FXMLLoader
+
+    @FXML // fx:id="cofreCBox"
+    private CheckBox cofreCBox; // Value injected by FXMLLoader
+
+    @FXML // fx:id="tvLedCBox"
+    private CheckBox tvLedCBox; // Value injected by FXMLLoader
+
+    @FXML // fx:id="telefoneCBox"
+    private CheckBox telefoneCBox; // Value injected by FXMLLoader
+
+    @FXML // fx:id="arCondCBox"
+    private CheckBox arCondCBox; // Value injected by FXMLLoader
+
+    @FXML // fx:id="wifiCBox"
+    private CheckBox wifiCBox; // Value injected by FXMLLoader
+
+    @FXML // fx:id="miniBarCBox"
+    private CheckBox miniBarCBox; // Value injected by FXMLLoader
+
+    @FXML // fx:id="disponivelCBox"
+    private CheckBox disponivelCBox; // Value injected by FXMLLoader
+
+    @FXML // fx:id="cancelarButton"
+    private Button cancelarButton; // Value injected by FXMLLoader
+
+    @FXML // fx:id="cadastrarButton"
+    private Button cadastrarButton; // Value injected by FXMLLoader
+
+    @FXML
+    void action(ActionEvent event) {
+
+    }
+	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		// TODO Auto-generated method stub
--- src/br/com/sistemahoteleiro/facade/Facade.java
+++ src/br/com/sistemahoteleiro/facade/Facade.java
@@ -99,6 +99,8 @@ public class Facade implements IFacade {
 		
 	}
 
+	//Administrador
+	
 	@Override
 	public void createOrUpdateAdministrador(Administrador administrador) throws BusinessException {
 		// TODO Auto-generated method stub
@@ -108,385 +110,409 @@ public class Facade implements IFacade {
 	@Override
 	public Administrador searchAdministrador(Administrador administrador, int id) throws BusinessException {
 		// TODO Auto-generated method stub
-		return null;
+		return businessAdministrador.search(Administrador.class, id);
 	}
 
 	@Override
 	public List<Administrador> searchAllAdministrador(Administrador administrador) throws BusinessException {
 		// TODO Auto-generated method stub
-		return null;
+		return businessAdministrador.searchAll(Administrador.class);
 	}
 
 	@Override
 	public void removeAdministrador(Administrador administrador) throws BusinessException {
 		// TODO Auto-generated method stub
-		
+		businessAdministrador.remove(administrador);
 	}
 
 	@Override
 	public void disableAdministrador(Administrador administrador) throws BusinessException {
 		// TODO Auto-generated method stub
-		
+		businessAdministrador.disable(administrador);
 	}
 
+	//Aluga
+	
 	@Override
 	public void createOrUpdateAluga(Aluga aluga) throws BusinessException {
 		// TODO Auto-generated method stub
-		
+		businessAluga.createOrUpdate(aluga);
 	}
 
 	@Override
 	public Aluga searchAluga(Aluga aluga, int id) throws BusinessException {
 		// TODO Auto-generated method stub
-		return null;
+		return businessAluga.search(Aluga.class, id);
 	}
 
 	@Override
 	public List<Aluga> searchAllAluga(Aluga aluga) throws BusinessException {
 		// TODO Auto-generated method stub
-		return null;
+		return businessAluga.searchAll(Aluga.class);
 	}
 
 	@Override
 	public void removeAluga(Aluga aluga) throws BusinessException {
 		// TODO Auto-generated method stub
-		
+		businessAluga.remove(aluga);
 	}
 
 	@Override
 	public void disableAluga(Aluga aluga) throws BusinessException {
 		// TODO Auto-generated method stub
-		
+		businessAluga.disable(aluga);
 	}
 
+	//Caixa
+	
 	@Override
 	public void createOrUpdateCaixa(Caixa caixa) throws BusinessException {
 		// TODO Auto-generated method stub
-		
+		businessCaixa.createOrUpdate(caixa);
 	}
 
 	@Override
 	public Caixa searchCaixa(Caixa caixa, int id) throws BusinessException {
 		// TODO Auto-generated method stub
-		return null;
+		return businessCaixa.search(Caixa.class, id);
 	}
 
 	@Override
 	public List<Caixa> searchAllCaixa(Caixa caixa) throws BusinessException {
 		// TODO Auto-generated method stub
-		return null;
+		return businessCaixa.searchAll(Caixa.class);
 	}
 
 	@Override
 	public void removeCaixa(Caixa caixa) throws BusinessException {
 		// TODO Auto-generated method stub
-		
+		businessCaixa.remove(caixa);
 	}
 
 	@Override
 	public void disableCaixa(Caixa caixa) throws BusinessException {
 		// TODO Auto-generated method stub
-		
+		businessCaixa.disable(caixa);
 	}
 
+	//Contato
+	
 	@Override
 	public void createOrUpdateContato(Contato contato) throws BusinessException {
 		// TODO Auto-generated method stub
-		
+		businessContato.createOrUpdate(contato);
 	}
 
 	@Override
 	public Contato searchContato(Contato contato, int id) throws BusinessException {
 		// TODO Auto-generated method stub
-		return null;
+		return businessContato.search(Contato.class, id);
 	}
 
 	@Override
 	public List<Contato> searchAllContato(Contato contato) throws BusinessException {
 		// TODO Auto-generated method stub
-		return null;
+		return businessContato.searchAll(Contato.class);
 	}
 
 	@Override
 	public void removeContato(Contato contato) throws BusinessException {
 		// TODO Auto-generated method stub
-		
+		businessContato.remove(contato);
 	}
 
 	@Override
 	public void disableContato(Contato contato) throws BusinessException {
 		// TODO Auto-generated method stub
-		
+		businessContato.disable(contato);
 	}
 
+	//Endereco
+	
 	@Override
 	public void createOrUpdateEndereco(Endereco endereco) throws BusinessException {
 		// TODO Auto-generated method stub
-		
+		businessEndereco.createOrUpdate(endereco);
 	}
 
 	@Override
 	public Endereco searchEndereco(Endereco endereco, int id) throws BusinessException {
 		// TODO Auto-generated method stub
-		return null;
+		return businessEndereco.search(Endereco.class, id);
 	}
 
 	@Override
 	public List<Endereco> searchAllEndereco(Endereco endereco) throws BusinessException {
 		// TODO Auto-generated method stub
-		return null;
+		return businessEndereco.searchAll(Endereco.class);
 	}
 
 	@Override
 	public void removeEndereco(Endereco endereco) throws BusinessException {
 		// TODO Auto-generated method stub
-		
+		businessEndereco.remove(endereco);
 	}
 
 	@Override
 	public void disableEndereco(Endereco endereco) throws BusinessException {
 		// TODO Auto-generated method stub
-		
+		businessEndereco.disable(endereco);
 	}
 
+	//Funcionario
+	
 	@Override
 	public void createOrUpdateFuncionario(Funcionario funcionario) throws BusinessException {
 		// TODO Auto-generated method stub
-		
+		businessFuncionario.createOrUpdate(funcionario);
 	}
 
 	@Override
 	public Funcionario searchFuncionario(Funcionario funcionario, int id) throws BusinessException {
 		// TODO Auto-generated method stub
-		return null;
+		return businessFuncionario.search(Funcionario.class, id);
 	}
 
 	@Override
 	public List<Funcionario> searchAllFuncionario(Funcionario funcionario) throws BusinessException {
 		// TODO Auto-generated method stub
-		return null;
+		return businessFuncionario.searchAll(Funcionario.class);
 	}
 
 	@Override
 	public void removeFuncionario(Funcionario funcionario) throws BusinessException {
 		// TODO Auto-generated method stub
-		
+		businessFuncionario.remove(funcionario);
 	}
 
 	@Override
 	public void disableFuncionario(Funcionario funcionario) throws BusinessException {
 		// TODO Auto-generated method stub
-		
+		businessFuncionario.disable(funcionario);
 	}
 
+	//Log
+	
 	@Override
 	public void createOrUpdateLog(Log log) throws BusinessException {
 		// TODO Auto-generated method stub
-		
+		businessLog.createOrUpdate(log);
 	}
 
 	@Override
 	public Log searchLog(Log log, int id) throws BusinessException {
 		// TODO Auto-generated method stub
-		return null;
+		return businessLog.search(Log.class, id);
 	}
 
 	@Override
 	public List<Log> searchAllLog(Log log) throws BusinessException {
 		// TODO Auto-generated method stub
-		return null;
+		return businessLog.searchAll(Log.class);
 	}
 
 	@Override
 	public void removeLog(Log log) throws BusinessException {
 		// TODO Auto-generated method stub
-		
+		businessLog.remove(log);
 	}
 
 	@Override
 	public void disableLog(Log log) throws BusinessException {
 		// TODO Auto-generated method stub
-		
+		businessLog.disable(log);
 	}
 
+	//Parcela
+	
 	@Override
 	public void createOrUpdateParcela(Parcela parcela) throws BusinessException {
 		// TODO Auto-generated method stub
-		
+		businessParcela.createOrUpdate(parcela);
 	}
 
 	@Override
 	public Parcela searchParcela(Parcela parcela, int id) throws BusinessException {
 		// TODO Auto-generated method stub
-		return null;
+		return businessParcela.search(Parcela.class, id);
 	}
 
 	@Override
 	public List<Parcela> searchAllParcela(Parcela parcela) throws BusinessException {
 		// TODO Auto-generated method stub
-		return null;
+		return businessParcela.searchAll(Parcela.class);
 	}
 
 	@Override
 	public void removeParcela(Parcela parcela) throws BusinessException {
 		// TODO Auto-generated method stub
-		
+		businessParcela.remove(parcela);
 	}
 
 	@Override
 	public void disableParcela(Parcela parcela) throws BusinessException {
 		// TODO Auto-generated method stub
-		
+		businessParcela.disable(parcela);
 	}
 
+	//PessoaFisica
+	
 	@Override
 	public void createOrUpdatePessoaFisica(PessoaFisica pessoaFisica) throws BusinessException {
 		// TODO Auto-generated method stub
-		
+		businessPessoaFisica.createOrUpdate(pessoaFisica);
 	}
 
 	@Override
 	public PessoaFisica searchPessoaFisica(PessoaFisica pessoaFisica, int id) throws BusinessException {
 		// TODO Auto-generated method stub
-		return null;
+		return businessPessoaFisica.search(PessoaFisica.class, id);
 	}
 
 	@Override
 	public List<PessoaFisica> searchAllPessoaFisica(PessoaFisica pessoaFisica) throws BusinessException {
 		// TODO Auto-generated method stub
-		return null;
+		return businessPessoaFisica.searchAll(PessoaFisica.class);
 	}
 
 	@Override
 	public void removePessoaFisica(PessoaFisica pessoaFisica) throws BusinessException {
 		// TODO Auto-generated method stub
-		
+		businessPessoaFisica.remove(pessoaFisica);
 	}
 
 	@Override
 	public void disablePessoaFisica(PessoaFisica pessoaFisica) throws BusinessException {
 		// TODO Auto-generated method stub
-		
+		businessPessoaFisica.disable(pessoaFisica);
 	}
 
+	//PessoaJuridica
+	
 	@Override
 	public void createOrUpdatePessoaJuridica(PessoaJuridica pessoaJuridica) throws BusinessException {
 		// TODO Auto-generated method stub
-		
+		businessPessoaJuridica.createOrUpdate(pessoaJuridica);
 	}
 
 	@Override
 	public PessoaJuridica searchPessoaJuridica(PessoaJuridica pessoaJuridica, int id) throws BusinessException {
 		// TODO Auto-generated method stub
-		return null;
+		return businessPessoaJuridica.search(PessoaJuridica.class, id);
 	}
 
 	@Override
 	public List<PessoaJuridica> searchAllPessoaJuridica(PessoaJuridica pessoaJuridica) throws BusinessException {
 		// TODO Auto-generated method stub
-		return null;
+		return businessPessoaJuridica.searchAll(PessoaJuridica.class);
 	}
 
 	@Override
 	public void removePessoaJuridica(PessoaJuridica pessoaJuridica) throws BusinessException {
 		// TODO Auto-generated method stub
-		
+		businessPessoaJuridica.remove(pessoaJuridica);
 	}
 
 	@Override
 	public void disablePessoaJuridica(PessoaJuridica pessoaJuridica) throws BusinessException {
 		// TODO Auto-generated method stub
-		
+		businessPessoaJuridica.disable(pessoaJuridica);
 	}
 
+	//Quarto
+	
 	@Override
 	public void createOrUpdateQuarto(Quarto quarto) throws BusinessException {
 		// TODO Auto-generated method stub
-		
+		businessQuarto.createOrUpdate(quarto);
 	}
 
 	@Override
 	public Quarto searchQuarto(Quarto quarto, int id) throws BusinessException {
 		// TODO Auto-generated method stub
-		return null;
+		return businessQuarto.search(Quarto.class, id);
 	}
 
 	@Override
 	public List<Quarto> searchAllQuarto(Quarto quarto) throws BusinessException {
 		// TODO Auto-generated method stub
-		return null;
+		return businessQuarto.searchAll(Quarto.class);
 	}
 
 	@Override
 	public void removeQuarto(Quarto quarto) throws BusinessException {
 		// TODO Auto-generated method stub
-		
+		businessQuarto.remove(quarto);
 	}
 
 	@Override
 	public void disableQuarto(Quarto quarto) throws BusinessException {
 		// TODO Auto-generated method stub
-		
+		businessQuarto.disable(quarto);
 	}
 
+	//Reserva
+	
 	@Override
 	public void createOrUpdateReserva(Reserva reserva) throws BusinessException {
 		// TODO Auto-generated method stub
-		
+		businessReserva.createOrUpdate(reserva);
 	}
 
 	@Override
 	public Reserva searchReserva(Reserva reserva, int id) throws BusinessException {
 		// TODO Auto-generated method stub
-		return null;
+		return businessReserva.search(Reserva.class, id);
 	}
 
 	@Override
 	public List<Reserva> searchAllReserva(Reserva reserva) throws BusinessException {
 		// TODO Auto-generated method stub
-		return null;
+		return businessReserva.searchAll(Reserva.class);
 	}
 
 	@Override
 	public void removeReserva(Reserva reserva) throws BusinessException {
 		// TODO Auto-generated method stub
-		
+		businessReserva.remove(reserva);
 	}
 
 	@Override
 	public void disableReserva(Reserva reserva) throws BusinessException {
 		// TODO Auto-generated method stub
-		
+		businessReserva.disable(reserva);
 	}
 
+	//SuperUsuario
+	
 	@Override
 	public void createOrUpdateSuperUsuario(SuperUsuario superUsuario) throws BusinessException {
 		// TODO Auto-generated method stub
-		
+		businessSuperUsuario.createOrUpdate(superUsuario);
 	}
 
 	@Override
 	public SuperUsuario searchSuperUsuario(SuperUsuario superUsuario, int id) throws BusinessException {
 		// TODO Auto-generated method stub
-		return null;
+		return businessSuperUsuario.search(SuperUsuario.class, id);
 	}
 
 	@Override
 	public List<SuperUsuario> searchAllSuperUsuario(SuperUsuario superUsuario) throws BusinessException {
 		// TODO Auto-generated method stub
-		return null;
+		return businessSuperUsuario.searchAll(SuperUsuario.class);
 	}
 
 	@Override
 	public void removeSuperUsuario(SuperUsuario superUsuario) throws BusinessException {
 		// TODO Auto-generated method stub
-		
+		businessSuperUsuario.remove(superUsuario);
 	}
 
 	@Override
 	public void disableSuperUsuario(SuperUsuario superUsuario) throws BusinessException {
 		// TODO Auto-generated method stub
-		
+		businessSuperUsuario.disable(superUsuario);
 	}
 	
 	
