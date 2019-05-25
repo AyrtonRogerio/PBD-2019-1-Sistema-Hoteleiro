c48007f4cc72310bacc96c64b5dcd3a9f0972a19
AyrtonRogerio <ayrton.rogerio1@gmail.com>
2019-05-25 13:50:56 -0300

    [Modificaçẽs no ControlerCliente e na view Cliente.]

--- src/br/com/sistemahoteleiro/app/App.java
+++ src/br/com/sistemahoteleiro/app/App.java
@@ -36,8 +36,8 @@ public class App extends Application {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		launch(args);
 		IFacade facade = Facade.getInstance();
+		launch(args);
 	}
 
 	@Override
@@ -103,16 +103,7 @@ public class App extends Application {
 	/**
 	 * @return the Pane quarto
 	 */
-/*	public static Pane getQuarto() {
-		return quarto;
-	}
 
-	/**
-	 * @return the Pane reserva
-	 */
-/*	public static Pane getReserva() {
-		return reserva;
-	}*/
 	
 	
 	
