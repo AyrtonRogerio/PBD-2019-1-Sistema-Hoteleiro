ebc3ce8ee5c0bdd719cb77e8f56018d30195c664
AyrtonRogerio <ayrton.rogerio1@gmail.com>
2019-05-26 15:21:11 -0300

    [Tela de login criada.]

---
+++ src/br/com/sistemahoteleiro/controler/ControlerLogin.java
@@ -0,0 +1,47 @@
+/**
+ * 
+ */
+package br.com.sistemahoteleiro.controler;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+/**
+ * @author ayrton
+ *
+ */
+public class ControlerLogin implements Initializable{
+
+	
+    @FXML
+    private JFXTextField loginField;
+
+    @FXML
+    private JFXPasswordField senhaField;
+
+    @FXML
+    private JFXButton entrarBtn;
+
+    @FXML
+    private JFXButton sairBtn;
+
+    @FXML
+    void action(ActionEvent event) {
+
+    }
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
